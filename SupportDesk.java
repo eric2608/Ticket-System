c920d9721be35f9389bcfa069fad777ef7114d6d
eric2608 <107226796+eric2608@users.noreply.github.com>
2025-11-02 20:10:50 -0500

    Implement SupportDesk class for ticket management

---
+++ SupportDesk.java
@@ -0,0 +1,101 @@
+package ticket.system;
+
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 13059
+ */
+public class SupportDesk {
+       Queue<Ticket> unsolvedTickets = new LinkedList<>();
+       Stack<Ticket> solvedIssues = new Stack<>();
+       static int idGenerator = 0;
+       
+       /*
+           • addTicket(Ticket t) → adds a new ticket to the queue.
+           • processNextTicket() → removes the next ticket from the queue and pushes it onto the stack.
+           • viewAllActiveTickets() → displays all tickets currently in the queue.
+           • viewRecentResolved() → shows the top 3 most recently resolved tickets (if available).
+           • reopenLastResolved() → pops a ticket from the stack and puts it back into the queue
+       */
+       
+       
+       public void addTicket(Ticket ticket){
+       
+                this.unsolvedTickets.add(ticket);
+       }
+       
+       public Ticket getTicket(){
+             Scanner scanner = new Scanner(System.in);
+             var id =idGenerator++;
+             String name;
+             String issue;
+            
+             System.out.println("Insert the name");
+             name = scanner.next();
+             
+             
+             System.out.println("Insert the issue ");
+             issue = scanner.next();
+             
+             
+             
+             return new Ticket(id,name,issue);
+       }
+       
+       
+       public void processNextTicket(){
+              
+            Ticket next = unsolvedTickets.poll(); 
+   
+             solvedIssues.push(next);
+       
+           
+       }
+       
+     
+       public void viewAllActiveTickets() {
+              if (unsolvedTickets.isEmpty()) {
+                  System.out.println(" No active tickets.");
+             return;
+          }
+        for (Ticket ticket : unsolvedTickets) {
+            System.out.println("--------- " + ticket.name + " --------");
+            System.out.println("ID:"+ticket.id+"--------");
+            System.out.println("Issue: " + ticket.issue);
+            System.out.println("Priority: " + ticket.priority);
+            System.out.println("----------------------------");
+        }
+    }
+        
+        
+          public void reopenLastResolve(){
+              
+            Ticket peek = solvedIssues.peek(); 
+   
+             unsolvedTickets.add(peek);
+       
+             solvedIssues.removeElementAt(0);
+           
+       }
+       
+       public void pop(){
+                System.out.println("---------"+solvedIssues.pop()+" --------");
+       }
+       
+       
+       
+      public void printTickets(){
+      
+               for (Ticket ticket : unsolvedTickets){
+               
+                       System.out.println(ticket.issue);
+                       System.out.println("---------------------------------------------");
+                    
+               }
+       
+      }
+}
